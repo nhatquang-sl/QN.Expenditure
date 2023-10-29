1965b64b038a148a30dcacbee1e5c4d401165265
Quang Nguyen <quang.sunlight@gmail.com>
2023-10-29 20:13:13 +0400

    feat(mediatr): ✨ use Task.WhenAll to handle notification

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MediatR.NotificationPublishers;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace Application
@@ -12,6 +13,7 @@ namespace Application
             services.AddMediatR(cfg =>
             {
                 cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                cfg.NotificationPublisher = new TaskWhenAllPublisher();
             });
 
             return services;
