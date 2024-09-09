587a1c4d979a58b28210ff8b5e05f90b7a815efa
Quang Nguyen <quang.sunlight@gmail.com>
2024-09-09 19:18:12 +0400

    feat(bnb): ✨ order by symbol and workingtime

--- src/Application/BnbSpotOrder/Queries/GetSpotOrders/GetSpotOrdersQuery.cs
+++ src/Application/BnbSpotOrder/Queries/GetSpotOrders/GetSpotOrdersQuery.cs
@@ -19,6 +19,8 @@ namespace Application.BnbSpotOrder.Queries.GetSpotOrders
         {
             var settings = await _applicationDbContext.SpotOrders
                 .Where(x => x.UserId == _currentUser.Id)
+                .OrderBy(x => x.Symbol)
+                .ThenByDescending(x => x.WorkingTime)
                 .ToListAsync(cancellationToken);
 
             return _mapper.Map<List<SpotOrderRaw>>(settings) ?? [];
