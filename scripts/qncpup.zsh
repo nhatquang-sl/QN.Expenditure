a28b5a9520b73e49877474a096c5f605d1e74788
Quang Nguyen <quang.nguyen@propelventures.ai>
2026-07-31 16:43:12 +0700

    chore: update qncpup script to use specific docker-compose files for deployment

--- scripts/qncpup.zsh
+++ scripts/qncpup.zsh
@@ -3,7 +3,7 @@ qncpup () {
     echo $curDir
     cd $QNEDIR
     
-    cmd="docker compose --env-file credentials/qex/.env up --build -d"
+    cmd="docker compose -f credentials/qex/docker-compose.yml -f credentials/qex/docker-compose.override.yml up --build -d"
     echo -e "\e[32m$cmd\e[0m"  # Green color for selected item
     eval $cmd
 
