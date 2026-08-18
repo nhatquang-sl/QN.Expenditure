7e4e8503f5a038d2f885261cf54dc4d5b73e8cdd
Quang Nguyen <quang.nguyen@propelventures.ai>
2026-08-19 00:36:59 +0700

    feat: add user table migration scripts and Docker support for database migrations

---
+++ golang/auth/internal/database/migrations/000001_create_users_table.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS Users (
+    "Id"                   text        NOT NULL PRIMARY KEY,
+    "UserName"             text        NOT NULL,
+    "NormalizedUserName"   text        NOT NULL,
+    "Email"                text        NOT NULL,
+    "NormalizedEmail"      text        NOT NULL,
+    "EmailConfirmed"       boolean     NOT NULL DEFAULT false,
+    "PasswordHash"         text        NOT NULL,
+    "SecurityStamp"        text        NOT NULL,
+    "ConcurrencyStamp"     text        NOT NULL,
+    "PhoneNumber"          text,
+    "PhoneNumberConfirmed" boolean     NOT NULL DEFAULT false,
+    "TwoFactorEnabled"     boolean     NOT NULL DEFAULT false,
+    "LockoutEnd"           timestamptz,
+    "LockoutEnabled"       boolean     NOT NULL DEFAULT false,
+    "AccessFailedCount"    integer     NOT NULL DEFAULT 0,
+    "FirstName"            text        NOT NULL,
+    "LastName"             text        NOT NULL
+);
