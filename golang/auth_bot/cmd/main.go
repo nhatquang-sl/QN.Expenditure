d9079a174c5be30d78a8e63fdf9fabf08bbf3d2b
Quang Nguyen <quang.nguyen@propelventures.ai>
2026-09-13 14:43:34 +0700

    feat(auth_bot): implement auth bot worker with Docker support and Makefile

---
+++ golang/auth_bot/cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	logger.Info("auth_bot started")
+	<-ctx.Done()
+	logger.Info("auth_bot shutting down")
+}
---
+++ golang/auth_bot/go.mod
@@ -0,0 +1,5 @@
+module auth_bot
+
+go 1.27.0
+
+replace qn.expenditure/shared => ../shared
